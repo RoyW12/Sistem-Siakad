e50382770e8e2d20a583d2dd2aadc1bf971264a0
RoyW12 <roywijaya893@gmail.com>
2023-09-16 10:18:37 +0700

    Roy deklarasi variabel 8-18

--- FiturUKT/App.java
+++ FiturUKT/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class App{
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
+                Scanner sc = new Scanner(System.in);
                 Long nim;
                 int jumlahTagihanUKT = 10000000,sisaTagihan,nominalPembayaran;
                 String nama;  
