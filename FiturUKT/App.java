7eeff4ba61624162a3ec8e14a1c460b5cfb3caaf
RoyW12 <roywijaya893@gmail.com>
2023-09-16 10:17:57 +0700

    Roy deklarasi variabel 8-18

---
+++ FiturUKT/App.java
@@ -0,0 +1,22 @@
+package FiturUKT;
+
+import java.util.Scanner;
+
+public class App{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+                Long nim;
+                int jumlahTagihanUKT = 10000000,sisaTagihan,nominalPembayaran;
+                String nama;  
+
+                System.out.println("Pembayaran UKT");
+                System.out.print("Nama Mahasiswa : ");
+                nama = sc.nextLine();
+                System.out.println("===============");
+                System.out.println("===============");
+                System.out.print("Nomor Induk Mahasiswa : ");
+
+
+    }
+}
