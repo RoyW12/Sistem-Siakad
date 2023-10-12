90070b5c1443005aa123b0ee163a4b2a99a90b7c
dinaarh <rahmawatidina0404@gmail.com>
2023-10-12 13:18:55 +0700

    Jadwal kuliahh

--- Fitur/JadwalKuliah.java
+++ Fitur/JadwalKuliah.java
@@ -22,7 +22,7 @@ public class JadwalKuliah {
             System.out.println("4. Kamis");
             System.out.println("5. Jumat");
 
-            System.out.print("Masukan Nomor = ");
+            System.out.print("Masukan Nomor Hari = ");
             Jadwal_MataKuliah = Masuk.nextInt();
 
             switch (Jadwal_MataKuliah) {
