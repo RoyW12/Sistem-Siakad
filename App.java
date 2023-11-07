693a74fc0c25dd18a049749ce4a7f98ba166db0e
dinaarh <rahmawatidina0404@gmail.com>
2023-11-07 14:20:29 +0700

    input matkul main

--- App.java
+++ App.java
@@ -14,6 +14,12 @@ public class App {
             { "1234560005", "Megawati", "1E", "Manajemen", "P" }
     };
 
+    static String[][] course = {
+            { "RTI231001", "Pancasila", "2" },
+            { "RTI231002", "Konsep Teknologi Informasi", "2" },
+            { "RTI231003", "Critical Thinking dan Problem Solving", "2" }
+    };
+
     public static void main(String[] args) {
 
         loginView();
@@ -203,6 +209,66 @@ public class App {
         System.out.println("+------+------------+----------------------+-------+----------------------+-----+");
     }
 
+    static void inputDataMatkul() {
+        String courseCode, courseName, sks;
+        boolean isFind = false;
+        renderTitle("Press enter to continue...");
+        userInput.nextLine().trim();
+        clearConsole();
+        renderCourseTable("Course data", course);
+        while (!isFind) {
+            System.out.print("Course Code       :");
+            courseCode = userInput.nextLine();
+            System.out.print("Course Name       :");
+            courseName = userInput.nextLine();
+            System.out.print("SKS               :");
+            sks = userInput.nextLine();
+
+            if (courseCode.length() == 9) {
+                for (int i = 0; i < course.length; i++) {
+                    if (courseCode.equals(course[i][0])) {
+                        clearConsole();
+                        System.out.println("Course with the Course code of " + courseCode + " already exists!");
+                        System.out.println("Masukkan data kembali");
+                        isFind = false;
+                        break;
+                    } else {
+                        isFind = true;
+                    }
+                    ;
+                }
+            } else {
+                clearConsole();
+                System.out.println("Kode Mata Kuliah harus 9 digit");
+            }
+            if (isFind) {
+                String[][] newCourse = new String[course.length + 1][3];
+                for (int i = 0; i < course.length; i++) {
+                    newCourse[i] = course[i];
+                }
+                newCourse[newCourse.length - 1] = new String[] { courseCode, courseName, sks };
+                course = newCourse;
+                renderCourseTable("Course data", course);
+                renderTitle("press enter to continue...");
+                userInput.nextLine().trim();
+                clearConsole();
+            }
+        }
+    }
+
+    static void renderCourseTable(String title, String[][] course) {
+        renderTitle(title);
+        System.out.println("+------+--------------------+----------------------------------------+-----+");
+        System.out.println("| No.  |    Course Code     |               Course Name              | SKS |");
+        System.out.println("+------+--------------------+----------------------------------------+-----+");
+        for (int i = 0; i < course.length; i++) {
+            String[] newCourse = course[i];
+            System.out.printf("| %-4d | %-18s | %-38s | %-3s |\n", (i + 1), newCourse[0], newCourse[1],
+                    newCourse[2]);
+        }
+        System.out.println("+------+--------------------+----------------------------------------+-----+");
+    }
+
     static void dashboardMahasiswa(String user) {
 
         while (true) {
@@ -226,10 +292,6 @@ public class App {
                     System.exit(choice);
             }
         }
-
-    }
-
-    static void inputDataMatkul() {
 
     }
 
