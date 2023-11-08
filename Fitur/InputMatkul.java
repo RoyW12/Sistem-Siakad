a1c4615d70e7225f13cfee1a9466bed4b92d2674
dinaarh <rahmawatidina0404@gmail.com>
2023-11-08 11:18:06 +0700

    modifikasi input matkul

--- Fitur/InputMatkul.java
+++ Fitur/InputMatkul.java
@@ -5,54 +5,77 @@ import java.util.Scanner;
 public class InputMatkul {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String username = "", password = "";
-        String jurusan = "";
-        String matkul = "";
-        int sks = 0;
-        int bobot = 0;
+        String[] kode = { "RTI231001", "RTI231002", "RTI231003", "RTI231004", "RTI231005" };
+        String[] matkul = { "Pancasila", "Konsep Teknologi Informasi", "Critical Thinking dan Problem Solving",
+                "Matdas", "Bahasa Inggris" };
+        String[] sks = { "2", "2", "2", "3", "2" };
 
-        // Login admin
-        System.out.print("Masukkan username : ");
-        username = input.nextLine();
-        System.out.print("Masukkan password : ");
-        password = input.nextLine();
+        String kodeInput;
+        String matkulInput;
+        String sksInput;
 
-        if (username.equals("admin") && password.equals("admin")) {
+        boolean isFind = false;
 
-            // Pilih jurusan
-            System.out.println("Pilih jurusan : ");
-            System.out.println("1. Teknik Informatika");
-            System.out.println("2. Sistem Informasi");
-            int pilihanJurusan = input.nextInt();
-
-            switch (pilihanJurusan) {
-                case 1:
-                    jurusan = "Teknik Informatika";
-                    break;
-                case 2:
-                    jurusan = "Sistem Informasi";
-                    break;
-                default:
-                    System.out.println("Pilihan jurusan tidak valid");
-                    return;
+        while (!isFind) {
+            System.out.println("+------+--------------------+----------------------------------------+-----+");
+            System.out.println("| No.  |    Course Code     |               Course Name              | SKS |");
+            System.out.println("+------+--------------------+----------------------------------------+-----+");
+            for (int i = 0; i < matkul.length; i++) {
+                System.out.printf("| %-4d | %-18s | %-38s | %-3s |\n", (i + 1), kode[i], matkul[i],
+                        sks[i]);
             }
+            System.out.println("+------+--------------------+----------------------------------------+-----+");
 
-            // Input data mata kuliah
-            System.out.println("Masukkan nama mata kuliah : ");
-            matkul = input.nextLine();
-            System.out.println("Masukkan jumlah sks : ");
-            sks = input.nextInt();
-            System.out.println("Masukkan bobot nilai : ");
-            bobot = input.nextInt();
+            System.out.print("Masukkan kode mata kuliah  : ");
+            kodeInput = input.nextLine();
+            System.out.print("Masukkan nama mata kuliah  : ");
+            matkulInput = input.nextLine();
+            System.out.print("Masukkan sks               : ");
+            sksInput = input.nextLine();
+            if (kodeInput.length() == 9) {
+                for (int i = 0; i < kode.length; i++) {
+                    if (kodeInput.equals(kode[i])) {
+                        System.out.println("Student with the Course code of " + kodeInput + " already exists!");
+                        System.out.println("Masukkan data lagi");
+                        isFind = false;
+                        break;
 
-            // Tampilkan data mata kuliah
-            System.out.println("Data mata kuliah : ");
-            System.out.println("Jurusan : " + jurusan);
-            System.out.println("Nama mata kuliah : " + matkul);
-            System.out.println("Jumlah sks : " + sks);
-            System.out.println("Bobot nilai : " + bobot);
-        } else {
-            System.out.println("Login gagal");
+                    } else {
+                        isFind = true;
+                    }
+                }
+
+            } else {
+                System.out.println("Kode Mata Kuliah harus 9 digit");
+            }
+            if (isFind) {
+                String[] newKode = new String[kode.length + 1];
+                String[] newMatkul = new String[matkul.length + 1];
+                String[] newSks = new String[sks.length + 1];
+
+                for (int i = 0; i < kode.length; i++) {
+                    newKode[i] = kode[i];
+                    newMatkul[i] = matkul[i];
+                    newSks[i] = sks[i];
+                }
+                newKode[newKode.length - 1] = kodeInput;
+                newMatkul[newMatkul.length - 1] = matkulInput;
+                newSks[newSks.length - 1] = sksInput;
+                kode = newKode;
+                matkul = newMatkul;
+                sks = newSks;
+
+                System.out.println("+------+--------------------+----------------------------------------+-----+");
+                System.out.println("| No.  |    Course Code     |               Course Name              | SKS |");
+                System.out.println("+------+--------------------+----------------------------------------+-----+");
+                for (int i = 0; i < matkul.length; i++) {
+                    System.out.printf("| %-4d | %-18s | %-38s | %-3s |\n", (i + 1), kode[i], matkul[i],
+                            sks[i]);
+                }
+                System.out.println("+------+--------------------+----------------------------------------+-----+");
+
+            }
         }
+
     }
 }
