cbc29e875782f870a928be6389a212a323c4b2a7
dinaarh <rahmawatidina0404@gmail.com>
2023-11-23 12:07:18 +0700

    khs but not finished yet

--- Fitur/Khs.java
+++ Fitur/Khs.java
@@ -5,9 +5,16 @@ import java.util.Scanner;
 public class Khs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String[] mataKuliah = new String[8];
+        String[] daftarMataKuliah = { "Pancasila", "Konsep Teknologi Informasi",
+                "Critical Thinking dan problem solving", "Matematika dasar", "Bahasa Inggris", "Dasar pemrograman",
+                "Praktikum daspro", "Keselamatan dan kesehatan kerja" };
+        String[] kodeMatkul = { "RTI231001", "RTI231002", "RTI231003", "RTI231004", "RTI231005", "RTI231006",
+                "RTI231007", "RTI231008" };
+        int sks[] = { 2, 3 };
         String nama = "", matkul = "", matkul1, matkul2, matkul3, matkul4, matkul5, predikat = "";
         String namaMaster = "dina";
-        long nim = 0;
+        int nim = 0;
         long nimMaster = 234172005;
         matkul1 = "PrakDaspro";
         matkul2 = "Daspro";
@@ -24,7 +31,7 @@ public class Khs {
             System.out.print("Masukkan nama    : ");
             nama = input.nextLine();
             System.out.print("Masukkan NIM     : ");
-            nim = input.nextLong();
+            nim = input.nextInt();
             input.nextLine();
 
             if (nama.equals(namaMaster)) {
@@ -39,7 +46,6 @@ public class Khs {
                 continue;
             }
         }
-
 
         int i = 1;
         while (i <= 5) {
@@ -100,25 +106,47 @@ public class Khs {
             System.out.println("nilai ips tidak terdefinisi");
         }
 
-        System.out.println("------------------------------------------------------------------");
-        System.out.println("|\t\t\t  KARTU HASIL STUDI   \t\t\t|");
-        System.out.println("|----------------------------------------------------------------|");
-        System.out.println("| Nama          : " + nama);
-        System.out.println("| NIM           : " + nim);
-        System.out.println(
-                "| Mata Kuliah 1 : " + matkul1 + " dengan sks " + sks1 + " dengan bobot nilai " + bobotMatkul1);
-        System.out.println(
-                "| Mata Kuliah 2 : " + matkul2 + " dengan sks " + sks2 + " dengan bobot nilai " + bobotMatkul2);
-        System.out.println(
-                "| Mata Kuliah 3 : " + matkul3 + " dengan sks " + sks3 + " dengan bobot nilai " + bobotMatkul3);
-        System.out.println(
-                "| Mata Kuliah 4 : " + matkul4 + " dengan sks " + sks4 + " dengan bobot nilai " + bobotMatkul4);
-        System.out.println(
-                "| Mata Kuliah 5 : " + matkul5 + " dengan sks " + sks5 + " dengan bobot nilai " + bobotMatkul5);
-        System.out.println("| ips           : " + ips + "  \t\t\t\t\t\t |");
-        System.out.println("|________________________________________________________________|");
-        System.out.println("| Status anda adalah " + predikat + "\t\t\t |");
-        System.out.println("|________________________________________________________________|");
+        System.out.println("+-------------------------------------------------------------------------");
+        System.out.println("| KARTU HASIL STUDI   %-10s|");
+        System.out.println("+-------------------------------------------------------------------------");
+        System.out.println("| Nama        : " + nama + " %-10s|");
+        System.out.println("| Nim        : " + nim + " %-10s|");
+        System.out.println("+------+------------+----------------------+-------+----------------------+-----+");
+        System.out.println("| No.  |    Kode Matkul     |      Mata Kuliah     | SKS |    Nilai Huruf    | Nilai |");
+        System.out.println("+------+------------+----------------------+-------+----------------------+-----+");
+        // for (int i = 0; i < nama.length; i++) {
+        // String[] student = nama[i];
+        // System.out.printf("| %-4d | %-10s | %-20s | %-5s | %-20s | %s |\n", (i + 1),
+        // student[0], student[1],
+        // student[2],
+        // student[3], student[4]);
+        // }
+        // System.out.println("+------+------------+----------------------+-------+----------------------+-----+");
+
+        // System.out.println("------------------------------------------------------------------");
+        // System.out.println("|\t\t\t KARTU HASIL STUDI \t\t\t|");
+        // System.out.println("|----------------------------------------------------------------|");
+        // System.out.println("| Nama : " + nama);
+        // System.out.println("| NIM : " + nim);
+        // System.out.println(
+        // "| Mata Kuliah 1 : " + matkul1 + " dengan sks " + sks1 + " dengan bobot nilai
+        // " + bobotMatkul1);
+        // System.out.println(
+        // "| Mata Kuliah 2 : " + matkul2 + " dengan sks " + sks2 + " dengan bobot nilai
+        // " + bobotMatkul2);
+        // System.out.println(
+        // "| Mata Kuliah 3 : " + matkul3 + " dengan sks " + sks3 + " dengan bobot nilai
+        // " + bobotMatkul3);
+        // System.out.println(
+        // "| Mata Kuliah 4 : " + matkul4 + " dengan sks " + sks4 + " dengan bobot nilai
+        // " + bobotMatkul4);
+        // System.out.println(
+        // "| Mata Kuliah 5 : " + matkul5 + " dengan sks " + sks5 + " dengan bobot nilai
+        // " + bobotMatkul5);
+        // System.out.println("| ips : " + ips + " \t\t\t\t\t\t |");
+        // System.out.println("|________________________________________________________________|");
+        // System.out.println("| Status anda adalah " + predikat + "\t\t\t |");
+        // System.out.println("|________________________________________________________________|");
 
     }
 }
