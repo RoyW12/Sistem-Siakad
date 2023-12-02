5985cc11ceb4d189e7fe386bec91e95df022bd5d
RoyW12 <roywijaya893@gmail.com>
2023-12-02 16:51:52 +0700

    perf: changed the every task into function

--- Fitur/updateDataMahasiswa.java
+++ Fitur/updateDataMahasiswa.java
@@ -3,16 +3,21 @@ package Fitur;
 import java.util.Scanner;
 
 public class updateDataMahasiswa {
-    public static void main(String[] args) {
-        Scanner sc28 = new Scanner(System.in);
+    static Scanner sc28 = new Scanner(System.in);
 
-        String[][] students = {
-                { "1234560001", "Roy wijaya", "1A", "Teknik Informatika", "L" },
-                { "1234560002", "Berta Christabel", "1B", "Teknik Informatika", "P" },
-                { "1234560003", "Joko Widodo", "1C", "Teknik Informatika", "L" },
-                { "1234560004", "Ganjar Pranowo", "1D", "Teknik Informatika", "L" },
-                { "1234560005", "Megawati", "1E", "Manajemen", "P" }
-        };
+    static String[][] students = {
+            { "1234560001", "Roy wijaya", "1A", "TI", "L" },
+            { "1234560002", "Berta Christabel", "1B", "TI", "P" },
+            { "1234560003", "Joko Widodo", "1C", "TI", "L" },
+            { "1234560004", "Ganjar Pranowo", "1D", "TI", "L" },
+            { "1234560005", "Megawati", "1E", "TI", "P" }
+    };
+
+    public static void main(String[] args) {
+        updateDataMahasiswa();
+    }
+
+    static void updateDataMahasiswa() {
         String newName = null;
         String nimInput;
         String newNIM = null;
@@ -41,6 +46,7 @@ public class updateDataMahasiswa {
                     if (nimInput.equals(students[i][0])) {
                         indexStudent = i;
                         isFind = true;
+                        System.out.println("student found");
                         break;
                     }
                 }
@@ -49,17 +55,14 @@ public class updateDataMahasiswa {
             }
 
             if (isFind) {
-                System.out.print("Masukkan nama mahasiswa           : ");
+                System.out.print("Masukkan nama baru           : ");
                 newName = sc28.nextLine();
-                System.out.print("Masukkan nim baru                 : ");
-                newNIM = sc28.nextLine();
-                System.out.print("Masukkan kelas mahasiswa          : ");
+                System.out.print("Masukkan kelas baru          : ");
                 newClass = sc28.nextLine();
-                System.out.print("Masukkan program studi mahasiswa  : ");
+                System.out.print("Masukkan program studi baru  : ");
                 newStudyProgram = sc28.nextLine();
 
                 students[indexStudent][0] = newName;
-                students[indexStudent][1] = newNIM;
                 students[indexStudent][2] = newClass;
                 students[indexStudent][3] = newStudyProgram;
                 System.out.println("+------+------------+----------------------+-------+----------------------+-----+");
